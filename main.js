6d62125aba6c193d8c28c95330e025b44b0a7d15
Anazodo Vanessa <anazodovanessa19@gmail.com>
2024-03-01 23:55:58 +0100

    educate

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
 });
+
+//show/hide faqs answer
+const faqs = document.querySelectorAll("faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+  });
+});
